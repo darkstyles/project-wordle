eda5aa8a0c7d44c98d6af3eff5fb4d5139fe74fb
PedroRoss <darkstylese@gmail.com>
2023-10-24 21:09:22 +0900

    exercise 3,4,5 and strech goal

--- src/components/GuessResult/GuessResult.js
+++ src/components/GuessResult/GuessResult.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
+import Guess from "../Guess/Guess";
 
 function GuessResult({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess) => (
-        <p className="guess">{guess}</p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
+        <Guess guess={guesses[index]} key={`${guesses[index]}-${index}`} />
       ))}
     </div>
   );
